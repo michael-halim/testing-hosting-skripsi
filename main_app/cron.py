1e9e965a2e3af33aaa3607e9c534928747bcb73c
Michael Halim <michaelhalimsenatra@gmail.com>
2023-01-24 19:40:31 +0700

    Try Error in Production #2

--- main_app/cron.py
+++ main_app/cron.py
@@ -110,15 +110,22 @@ def train_als_model(user_id, is_refresh_time_based):
         ucf_model = load_model(dest_path)
 
     else:
-        
         print_help(var='TRAINING UCF MODEL', username='TRAIN ALS MODEL')
+        
         # Train UCF Model
         ucf_model = AlternatingLeastSquares(factors=64, regularization=0.05)
         ucf_model.fit(2 * user_product_matrix)
 
         dump_model(ucf_model, dest_path)
 
-    print_help(var=creation_date(dest_path), title='CREATION OR MODIFIED DATE FILE', username='SERVER TRAINING')
+    epoch_time = creation_date(dest_path)
+    current_time = datetime.fromtimestamp(epoch_time)
+    zone_time = current_time.astimezone(ZoneInfo('Asia/Bangkok')) 
+    
+    print_help(var=creation_date(dest_path), title='EPOCH TIME CREATION OR MODIFIED DATE FILE', username='SERVER TRAINING')
+    print_help(var=current_time, title='CURRENT TIME CREATION OR MODIFIED DATE FILE', username='SERVER TRAINING')
+    print_help(var=zone_time, title='ZONE TIME CREATION OR MODIFIED DATE FILE', username='SERVER TRAINING')
+    
     
     ids, scores = ucf_model.recommend(user_id, user_product_matrix[user_id], N=50, filter_already_liked_items=False)
 
@@ -211,39 +218,35 @@ def train_weighted_matrix(user_id , total_highest_cbf = TOTAL_HIGHEST_CBF, total
     for _id in cbf_product_ids:
         distance_object = Distance.objects.filter(Q(product_id =_id) | Q(other_product_id = _id))[:1]
         
-        try:
-            if distance_object[0].feature_added == FEATURES and distance_object[0].temp_distance != 99.0:
-                print('CONTINUE')
-                continue
-            
-            else:
-                distance_object = Distance.objects.filter(Q(product_id =_id) | Q(other_product_id = _id))
+        if distance_object[0].feature_added == FEATURES and distance_object[0].temp_distance != 99.0:
+            print('CONTINUE')
+            continue
+        
+        else:
+            distance_object = Distance.objects.filter(Q(product_id =_id) | Q(other_product_id = _id))
 
-                for obj in distance_object:
-                    minus = 0
+            for obj in distance_object:
+                minus = 0
 
-                    if not NAME_FEATURE:
-                        minus += obj.name_distance
-                    if not PRICE_FEATURE:
-                        minus += obj.price_distance
-                    if not DESCRIPTION_FEATURE:
-                        minus += obj.description_distance
-                    if not MATERIAL_FEATURE:
-                        minus += obj.material_distance
-                    if not WEIGHT_FEATURE:
-                        minus += obj.weight_distance
-                    if not DIMENSION_FEATURE:
-                        minus += obj.dimension_distance
-                    if not FURNITURE_LOCATION_FEATURE:
-                        minus += obj.furniture_location_distance
+                if not NAME_FEATURE:
+                    minus += obj.name_distance
+                if not PRICE_FEATURE:
+                    minus += obj.price_distance
+                if not DESCRIPTION_FEATURE:
+                    minus += obj.description_distance
+                if not MATERIAL_FEATURE:
+                    minus += obj.material_distance
+                if not WEIGHT_FEATURE:
+                    minus += obj.weight_distance
+                if not DIMENSION_FEATURE:
+                    minus += obj.dimension_distance
+                if not FURNITURE_LOCATION_FEATURE:
+                    minus += obj.furniture_location_distance
 
-                    obj.temp_distance = obj.total_distance - minus
-                    obj.feature_added = FEATURES
-                    obj.save()
+                obj.temp_distance = obj.total_distance - minus
+                obj.feature_added = FEATURES
+                obj.save()
 
-        except IndexError as e:
-            print(e)
-            print_help(var=distance_object, title='CHECK ERROR', username='SERVER TRAINING')
 
     cbf_highest_item_list = []
     cbf_lowest_item_list = []
